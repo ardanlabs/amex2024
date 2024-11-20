79d99ac050f7046257a4a0307362682bfc482a80
William Kennedy <bill@ardanlabs.com>
2024-11-20 13:11:58 -0700

    commit 21

--- app/domain/testapp/testapp.go
+++ app/domain/testapp/testapp.go
@@ -5,13 +5,14 @@ import (
 	"math/rand/v2"
 	"net/http"
 
+	"github.com/ardanlabs/service/app/sdk/errs"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
 func test(ctx context.Context, r *http.Request) web.Encoder {
 	if n := rand.IntN(100); n%2 == 0 {
-		//return errs.Newf(errs.InvalidArgument, "you sent me bad stuff")
-		panic("THIS IS A PANIC")
+		return errs.Newf(errs.InvalidArgument, "you sent me bad stuff")
+		//panic("THIS IS A PANIC")
 	}
 
 	resp := status{
