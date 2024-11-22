a5fadc02b3d63e7d759805ea0934363557de462c
William Kennedy <bill@ardanlabs.com>
2024-11-22 09:46:20 -0700

    commit 33

---
+++ business/sdk/unitest/unitest.go
@@ -0,0 +1,27 @@
+package unitest
+
+import (
+	"context"
+	"testing"
+)
+
+func Run(t *testing.T, table []Table, testName string) {
+	for _, tt := range table {
+		f := func(t *testing.T) {
+			gotResp := tt.ExcFunc(context.Background())
+
+			diff := tt.CmpFunc(gotResp, tt.ExpResp)
+			if diff != "" {
+				t.Log("DIFF")
+				t.Logf("%s", diff)
+				t.Log("GOT")
+				t.Logf("%#v", gotResp)
+				t.Log("EXP")
+				t.Logf("%#v", tt.ExpResp)
+				t.Fatalf("Should get the expected response")
+			}
+		}
+
+		t.Run(testName+"-"+tt.Name, f)
+	}
+}
